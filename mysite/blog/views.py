5dfe45e12f918e5588763b7e7b1d362b7df4f9ec
sofijasljusar <sofija.sljusar@gmail.com>
2025-01-08 20:07:53 +0200

    Initial commit

---
+++ mysite/blog/views.py
@@ -0,0 +1,19 @@
+# from django.shortcuts import render
+from django.views import generic
+from .models import Post
+# Create your views here.
+
+
+class PostList(generic.ListView):
+    """
+    Return all posts that are with status 1 (published) and order from the latest one
+    """
+    queryset = Post.objects.filter(status=1).order_by('-created_at')
+    template_name = 'index.html'
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
