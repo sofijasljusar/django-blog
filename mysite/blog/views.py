a743ee832a754d54ad080247411e38379cc2ba9a
sofijasljusar <sofija.sljusar@gmail.com>
2025-06-06 12:23:00 +0300

    Add dynamin header images and fix button

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,54 +1,12 @@
 # from django.shortcuts import render
+import requests
 from django.views import generic
 
 # from .models import Post
 # Create your views here.
 
 # Hardcoded blog data (simulating database records)
-BLOG_POSTS = [
-    {
-        'title': 'Можлива причина дивної ходьби: У пінгвінів немає колін?',
-        'slug': 'penguin-knees',
-        'author': '',
-        'status': 'Publish',
-        'created_at': '23 лютого 2025 р. 15:00',
-        'updated_at': '23 лютого 2025 р. 15:00',
-        'content': [
-            {"type": "paragraph", "text": "Багато хто вважає, що у пінгвінів немає колін, але це не так."},
-
-            {"type": "header", "text": "Чому пінгвіни так ходять?"},
-            {"type": "paragraph", "text": "Багато хто вважає, що у пінгвінів немає колін, але це не так."},
-            {"type": "image", "url": "https://example.com/penguin.jpg", "caption": "Пінгвін на льоду"},
-            {"type": "list", "items": [
-                "Їхні ноги дуже короткі.",
-                "Спеціальний механізм ходьби зберігає енергію.",
-                "Центр ваги знаходиться близько до землі."
-            ]},
-            {"type": "quote", "text": "Пінгвіни – це унікальні птахи, які адаптувалися до холодного клімату.", "author": "Дослідник Антарктиди"}
-        ],
-    },
-    {'title': 'Чи є коліна у качок? Як качки висиджують яйця, якщо у них немає колін?',
-     'slug': 'duck-knees',
-     'author': '',
-     'content': 'This is the content for post 1.',
-     'status': 'Publish',
-     'created_at': '23 лютого 2025 р. 17:00',
-     'updated_at': '23 лютого 2025 р. 17:00'},
-    {'title': 'Як курка не падає на двох ногах? Чи є у курки коліна?',
-     'slug': 'chicken-knees',
-     'author': '',
-     'content': 'This is the content for post 1.',
-     'status': 'Publish',
-     'created_at': '23 лютого 2025 р. 19:00',
-     'updated_at': '23 лютого 2025 р. 19:00'},
-    {'title': 'Так багато запитань і так мало відповідей...',
-     'slug': 'answers',
-     'author': '',
-     'content': 'This is the content for post 1.',
-     'status': 'Publish',
-     'created_at': '23 лютого 2025 р. 21:30',
-     'updated_at': '23 лютого 2025 р. 21:30'},
-]
+BLOG_POSTS = requests.get('https://api.npoint.io/ce3b8ba44b768e2a827e').json()
 
 
 # class PostList(generic.ListView):
